5f3d697de517b55a53afdb12db922dfb3e3834df
anisha <merlinanisha54@gmail.com>
2026-03-23 12:57:03 +0530

    initail commit

---
+++ students result using (collabration).py
@@ -0,0 +1,10 @@
+def student_result(name,marks):
+    if marks>=50:
+        print(name,"pass")
+    else:
+        print(name,"fail")
+student_result("madhu",100)
+student_result("anisha",95)
+student_result("kavya",35)
+student_result("selva",75)
+student_result("anita",40)
